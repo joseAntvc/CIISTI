d62b426a2e3cf1e0fb4bed47c38e5b7178a362ed
Heriberto Zuñiga Hernandez <21031011@itcelaya.edu.mx>
2024-10-14 07:17:53 -0600

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+
 
 Route::get('/users', [SiteController::class, 'users'])->name('users');
 Route::get('/form/{id?}', [SiteController::class, 'form'])->name('form');
